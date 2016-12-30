fcae896e04df8ffdaeb60091c3a9c62bacff4190
vincent Artino <vatsav23@gmail.com>
2016-12-31 01:13:00 +0530

    make it better

--- main.js
+++ main.js
@@ -1,19 +1,29 @@
 express = require('express');
 https = require('https'),  
 request = require('request');
-var bodyParser = require('body-parser')
-
 var app = express();
 port = Number(process.env.PORT || 5000);
+
+var bodyParser = require('body-parser')
 app.use(bodyParser.urlencoded({
     extended: true
 }));
 app.use(bodyParser.json());
 
-console.log("jhvd jd");
 
 app.post('/webhook', function (req, res) {
-	console.log(req.body.result.action)	 
+if(req.body.result.action == "weather"){
+	console.log("weather request");
+	city = weather(req);
+}
+
 });
-  
+
+function weather(req){
+	baseurl = "https://query.yahooapis.com/v1/public/yql?";
+	city = req.body.result.parameters;
+	console.log(city);
+	return city;
+}
+
 app.listen(port);
