10b7bc2100130e9881166f110a86a105a87e89cc
vincent Artino <vatsav23@gmail.com>
2016-12-31 01:33:04 +0530

    make it better

--- main.js
+++ main.js
@@ -20,13 +20,14 @@ if(req.body.result.action == "weather"){
 });
 
 function weather(req){
-	baseurl = "https://query.yahooapis.com/v1/public/yql?";
+	baseurl = "https://query.yahooapis.com/v1/public/yql?q=";
 	city = req.body.result.parameters["geo-city"];
 	if(city == null)
 		return {}
 	else{
 	query = "select * from weather.forecast where woeid in (select woeid from geo.places(1) where text='" + city + "')";
-	urlQuery = "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22nome%2C%20ak%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys";
+	urlQuery = baseurl + query + "&format=json";
+	console.log(query);
 	request({
 	    url: urlQuery,
 	    json: true
