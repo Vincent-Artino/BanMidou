15e315cde5f115a899026dadc917532ca5bbd0a4
vincent Artino <vatsav23@gmail.com>
2016-12-31 00:50:47 +0530

    make it better

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ port = Number(process.env.PORT || 5000);
 console.log("jhvd jd");
 
 app.post('/webhook', function (req, res) {
-	console.log(req.event)	 
+	console.log(JSON.stringify(req))	 
  /*// Make sure this is a page subscription
   if (data.object === 'page') {
 
