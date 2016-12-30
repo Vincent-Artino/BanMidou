b6072ef79856da9f4ea557dc8125ffa7854a3886
vincent Artino <vatsav23@gmail.com>
2016-12-31 00:59:36 +0530

    make it better

--- main.js
+++ main.js
@@ -13,30 +13,7 @@ app.use(bodyParser.json());
 console.log("jhvd jd");
 
 app.post('/webhook', function (req, res) {
-	console.log(req.body)	 
- /*// Make sure this is a page subscription
-  if (data.object === 'page') {
-
-    // Iterate over each entry - there may be multiple if batched
-    data.entry.forEach(function(entry) {
-      var pageID = entry.id;
-      var timeOfEvent = entry.time;
-
-      // Iterate over each messaging event
-      entry.messaging.forEach(function(event) {
-        if (event.message) {
-          receivedMessage(event);
-        } else {
-          console.log("Webhook received unknown event: ", event);
-        }
-      });
-    });
-    res.sendStatus(200);
-  }*/
+	console.log(req.body.action)	 
 });
   
-function receivedMessage(event) {
-  // Putting a stub for now, we'll expand it in the following steps
-  console.log("Message data: ", event.message);
-}  
 app.listen(port);
