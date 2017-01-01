0779024d8b2d1530278d092219e747a4d917c3ed
vincent Artino <vatsav23@gmail.com>
2017-01-01 22:15:20 +0530

    make it better

--- main.js
+++ main.js
@@ -95,7 +95,7 @@ function sendButtonMessage(text,res){
 	facebook :{
   	        "recipient":{
    	 		"id":"USER_ID"
-		  			},
+		  	},
 			  "message":{
 			    "attachment":{
 			      "type":"template",
@@ -103,21 +103,22 @@ function sendButtonMessage(text,res){
 				"template_type":"button",
 				"text":"What do you want to do next?",
 				"buttons":[
-				  {
-				    "type":"web_url",
-				    "url":"https://petersapparel.parseapp.com",
-				    "title":"Show Website"
-				  },
-				  {
-				    "type":"postback",
-				    "title":"Start Chatting",
-				    "payload":"USER_DEFINED_PAYLOAD"
-				  }
-				]
+					  {
+					    "type":"web_url",
+					    "url":"https://petersapparel.parseapp.com",
+					    "title":"Show Website"
+					  },
+					  {
+					    "type":"postback",
+					    "title":"Start Chatting",
+					    "payload":"USER_DEFINED_PAYLOAD"
+					  }
+					]
 			      }
 			    }
+			}
 		}	
-	}
+	},
         source : "item"
   	});
    	res.end(json);		
