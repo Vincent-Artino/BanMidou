8a8e020cd4f6b8d67d17e08f5682737db037ba4e
vincent Artino <vatsav23@gmail.com>
2017-01-17 14:44:57 +0530

    make it better

--- main.js
+++ main.js
@@ -34,7 +34,8 @@ else if(req.body.result.action == "duck"){
 	ddg.instantAnswer(str, {skip_disambig: '0'}, function(err, response) {
 	  console.log("yea ... ");	
 	  console.log(response);
-	  //sendMessage(response);
+	  console.log(response.body.RelatedTopics);	  
+	//sendMessage(response);
 	});	
 	/*ddg.query(str, options, function(err, data){
 	console.log("yea ... ");
