af54c6e48916fdd49adbbdaef88c954bc5b49aaf
vincent Artino <vatsav23@gmail.com>
2017-01-17 17:34:51 +0530

    make it better

--- main.js
+++ main.js
@@ -76,8 +76,8 @@ function duck(query,res){
 		if(body!=null){
 			console.log(body.Abstract);
 			console.log(body.Definition);
-			console.log(body.RelatedTopics[0].result);
-			sendMessage(body.RelatedTopics[0].result,res);	
+			console.log(body.RelatedTopics[0].Result);
+			sendMessage(body.RelatedTopics[0].Result,res);	
 		}	
 	}	
 });
