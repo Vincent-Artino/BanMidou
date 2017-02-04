d0e1b78a8344ad8d6a53d84afd1a8053385990ec
vincent Artino <vatsav23@gmail.com>
2017-02-04 12:47:56 +0530

    make it better

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ var app = express();
 port = Number(process.env.PORT || 5000);
 var city,text,temp,temperature;
 var bodyParser = require('body-parser')
+var PAGE_ACCESS_TOKEN = "EAASB0LLNZB9MBALW7cTkTxUkNYSurJG5jbxHcggTGMzTEqC5QjyxjifkGIrDJ7JCfZCQMjLRZCofQPiQbk1QoYnltqodzihlrZAdmLbIQVyUtP84gkdYuggrZBkqYeoDR3t5b1Eb8v5ueriOViPxTBZCDrLhxySSH1PkPD4GZBplgZDZD";
 app.use(bodyParser.urlencoded({
     extended: true
 }));
@@ -26,8 +27,9 @@ else if(req.body.result.action == "duck"){
 	console.log(str);
 	duck(str,res);
 } 
-else if(req.body.result.action == "test"){
-	sendGMessage("okay",res);
+else if(req.body.result.action == "hello"){
+	console.log("orey");	
+	callSendAPI("osey edava");
 }
 else if(req.body.result.action == "Gifs"){
 	console.log("hjdv skd");
@@ -158,11 +160,27 @@ function sendGMessage(text,res){
         displayText : text, 
      	 });
    	res.write(json);
-	var json1 = JSON.stringify({
-	speech : "okay aa",
-	displayText : "naah man"
-	})
-	res.write(json1);
-}
 
+}
+function callSendAPI(messageData) {
+  request({
+    uri: 'https://graph.facebook.com/v2.6/me/messages',
+    qs: { access_token: PAGE_ACCESS_TOKEN },
+    method: 'POST',
+    json: messageData
+
+  }, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      var recipientId = body.recipient_id;
+      var messageId = body.message_id;
+
+      console.log("Successfully sent generic message with id %s to recipient %s", 
+        messageId, recipientId);
+    } else {
+      console.error("Unable to send message.");
+      console.error(response);
+      console.error(error);
+    }
+  });  
+}
 app.listen(port);
